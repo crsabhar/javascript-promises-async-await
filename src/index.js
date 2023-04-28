3c4240624a13e89e6b3a9ecb6ee1502eadf738b2
Chetan Sabhar <chetan.sabhar@gmail.com>
2023-04-28 23:39:27 +0530

    Completed module 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { error } from 'jquery';
 import { fetchWithTimeout, fetchMovies, fetchBooks, asyncFetchMovies, asyncFetchBooks } from './services';
 const movies = require('./data/movies.json');
 
@@ -42,7 +43,7 @@ async function getBooksOrMoviesAsync() {
         const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
         return values;     
     } catch (error) {
-        console.log("Error waiting for the promise race", error);
+        console.error("Error waiting for the promise race", error);
     }
 }
 
@@ -51,10 +52,14 @@ getBooksAndMoviesAsync().then(results => {
         movies: results.movies,
         books: results.books
     });
+}).catch(error => {
+    console.error("Error in getBooksAndMoviesAsync execution", error);
 });
 
 getBooksOrMoviesAsync().then(results => {
     console.log("movies OR books", {
         results
     });
+}).catch(error => {
+    console.error("Error in getBooksOrMoviesAsync execution", error)
 });
